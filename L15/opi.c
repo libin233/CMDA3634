5f8a09942533d9ac6e94a3a4205957a25e3eb613
BinLi <binl8@vt.edu>
2018-03-13 16:43:24 -0400

    class exercise

--- L15/opi.c
+++ L15/opi.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <omp.h>
 int main(int argc, char **argv) {
 
   //seed random number generator
@@ -14,9 +14,13 @@ int main(int argc, char **argv) {
 
   // Q2c: add an OpenMP parallel region here, wherein each thread initializes 
   //      one entry in drandData using srand48_r and seed based on thread number
+#pragma omp parallel
+{
   long int seed = 0;
-  srand48_r(seed, drandData+0);
-
+  int rank = omp_get_thread_num();
+  srand48_r(seed, drandData+rank);
+  
+}
   long long int Ntrials = 10000000;
 
 
@@ -29,8 +33,9 @@ int main(int argc, char **argv) {
     double rand2;
 
     //gererate two random numbers (use the thread id to offset drandData)
-    drand48_r(drandData+0, &rand1);
-    drand48_r(drandData+0, &rand2);
+    int rank = omp_get_thread_num();
+    drand48_r(drandData+rank, &rand1);
+    drand48_r(drandData+rank, &rand2);
     
     double x = -1 + 2*rand1; //shift to [-1,1]
     double y = -1 + 2*rand2;
